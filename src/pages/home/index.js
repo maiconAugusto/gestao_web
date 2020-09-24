6c41885a7d6ae4fd6a6e1eed9deed7820cfad694
Maicon Augusto <maicon.augusto.carvalho@hotmail.com>
2020-09-24 11:37:58 -0400

    resolve home

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -2,35 +2,78 @@ import React, {useEffect, useState} from 'react';
 import SideMenu from '../../components/sideMenu/index'
 import {Container, Div} from './style.js';
 import Card from 'react-bootstrap/Card';
+import Spinner from 'react-bootstrap/Spinner';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faUser } from '@fortawesome/free-solid-svg-icons'
 import api from '../../services/api';
-import {useSelector} from 'react-redux';
 
 export default function Home () {
 
     let user  = JSON.parse(localStorage.getItem('@email'));
 
-    const [categories, setCategories] = useState([]);
-    const [total , settotal] = useState('0');
-    // useEffect(()=> {
-    //     getCategories();
-    //     getTotal();
-    // }, [])
+    let paciente = ''
+    let contratado = ''
+    let outros = ''
+    let voluntario = ''
 
-    // async function getCategories() {
-    //     await api.get('/categories')
-    //         .then((response)=> {
-    //             setCategories(response.data.data);
-    //             console.log(response.data.data)
-    //         })
-    // }
-    // async function getTotal () {
-    //     await api.get('/dashboard')
-    //     .then((response)=> {
-    //         settotal(response.data.data)
-    //     })
-    // }
+    const [loading, setLoading] = useState(false);
+    const [_total , settotal] = useState('');
+    const [_pacinete, setPaciente] = useState('');
+    const [_contratado, setContratado] = useState('');
+    const [_outros, setOutros] = useState('');
+    const [_voluntario, setVoluntario] = useState('');
+
+    useEffect(()=> {
+        getCategories();
+    }, [])
+
+    async function getCategories() {
+        setLoading(true);
+        await api.get('/categories')
+            .then((response)=> {
+                let aux = response.data.data;
+                aux.map(element => {
+                    if (element.categories === 'Paciente') {
+                        paciente = element.id;
+                    }
+                    if (element.categories === 'Contratado') {
+                        contratado = element.id;
+                    }
+                    if (element.categories === 'Outros') {
+                        outros = element.id;
+                    }
+                    if (element.categories === 'Voluntário') {
+                        voluntario = element.id;
+                    }
+                })
+            getTotal();
+            })
+            .catch(() => {
+                setLoading(false);
+            })
+    }
+    async function getTotal () {
+        await api.get('/total', {
+            params: {
+                paciente: paciente,
+                contratado: contratado,
+                outros: outros,
+                voluntario: voluntario
+            }
+        })
+        .then((response)=> {
+            settotal(response.data.data.total.count);
+            setOutros(response.data.data.outro.count);
+            setPaciente(response.data.data.pacientes.count);
+            setContratado(response.data.data.contratados.count);
+            setVoluntario(response.data.data.voluntarios.count);
+            setLoading(false);
+        })
+        .catch(() => {
+            setLoading(false);
+        })
+    }
     return (
         <div> 
             <SideMenu option={'home'} />
@@ -39,44 +82,61 @@ export default function Home () {
                 <small style={{marginRight: 6}}>Usúario: {user.name}</small>
             </div>
             <Container>
-                <Div>
-                    <div className="tag" style={{display: 'flex', marginBottom: 2}}>
-                        <Card style={{ width: '18rem', margin:6 }}>
-                            <Card.Body>
-                                <Card.Title style={{fontSize: 14}}>Total de colaboradores:</Card.Title>
-                                <Card.Text style={{fontSize: 14}}>
-                                Total: {total}
-                                </Card.Text>
-                            </Card.Body>
-                        </Card>
-                        <Card style={{ width: '18rem', margin:6  }}>
-                            <Card.Body>
-                                <Card.Title style={{fontSize: 14}}>Total de contratados:</Card.Title>
-                                <Card.Text style={{fontSize: 14}}>
-                                Total: 34
-                                </Card.Text>
-                            </Card.Body>
-                        </Card>
-                    </div>
-                    <div className="tag" style={{display: 'flex'}}>
-                        <Card style={{ width: '18rem', margin:6  }}>
-                            <Card.Body>
-                                <Card.Title style={{fontSize: 14}}>Total de voluntários:</Card.Title>
-                                <Card.Text style={{fontSize: 14}}>
-                                Total: 23
-                                </Card.Text>
-                            </Card.Body>
-                        </Card>
-                        <Card style={{ width: '18rem', margin:6  }}>
-                            <Card.Body>
-                                <Card.Title style={{fontSize: 14}}>Outros:</Card.Title>
-                                <Card.Text style={{fontSize: 14}}>
-                                Total: 45
-                                </Card.Text>
-                            </Card.Body>
-                        </Card>
-                    </div>
-                </Div>
+                    {loading === true ? (
+                        <div style={{paddingTop: 50, display: 'flex', alignItems: 'center'}}>
+                            <Spinner animation="border" variant="danger" />
+                            <small style={{marginLeft: 8}}>Carregando...</small>
+                        </div>
+                    ) : (
+                        <Div>
+                            <div className="tag" style={{display: 'flex', marginBottom: 2}}>
+                                <Card style={{ width: '18rem', margin:6 }}>
+                                    <Card.Body>
+                                        <Card.Title style={{fontSize: 14}}>Total de colaboradores:</Card.Title>
+                                        <Card.Text style={{fontSize: 14}}>
+                                        Total: {_total}
+                                        </Card.Text>
+                                    </Card.Body>
+                                </Card>
+                                <Card style={{ width: '18rem', margin:6  }}>
+                                    <Card.Body>
+                                        <Card.Title style={{fontSize: 14}}>Total de contratados:</Card.Title>
+                                        <Card.Text style={{fontSize: 14}}>
+                                        Total: {_contratado}
+                                        </Card.Text>
+                                    </Card.Body>
+                                </Card>
+                            </div>
+                            <div className="tag" style={{display: 'flex'}}>
+                                <Card style={{ width: '18rem', margin:6  }}>
+                                    <Card.Body>
+                                        <Card.Title style={{fontSize: 14}}>Total de voluntários:</Card.Title>
+                                        <Card.Text style={{fontSize: 14}}>
+                                        Total: {_voluntario}
+                                        </Card.Text>
+                                    </Card.Body>
+                                </Card>
+                                <Card style={{ width: '18rem', margin:6  }}>
+                                    <Card.Body>
+                                        <Card.Title style={{fontSize: 14}}>Outros:</Card.Title>
+                                        <Card.Text style={{fontSize: 14}}>
+                                        Total: {_outros}
+                                        </Card.Text>
+                                    </Card.Body>
+                                </Card>
+                            </div>
+                            <div className="tag" style={{display: 'flex'}}>
+                                <Card style={{ width: '18rem', margin:6  }}>
+                                    <Card.Body>
+                                        <Card.Title style={{fontSize: 14}}>Total de pacientes:</Card.Title>
+                                        <Card.Text style={{fontSize: 14}}>
+                                        Total: {_pacinete}
+                                        </Card.Text>
+                                    </Card.Body>
+                                </Card>
+                            </div>
+                        </Div>
+                    )}
             </Container>
         </div>
     )
