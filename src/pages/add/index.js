657e80c3da0083c36b35c3b53bf8eaa074341b43
Maicon Augusto <maicon.augusto.carvalho@hotmail.com>
2020-10-08 17:16:43 -0400

    ajuste

--- src/pages/add/index.js
+++ src/pages/add/index.js
@@ -14,6 +14,7 @@ import api from '../../services/api';
 import {useDispatch} from 'react-redux';
 import { useHistory } from "react-router-dom";
 
+
 export default function Add () {
     const [categories, setCategories] = useState([]);
     const dispatch = useDispatch();
