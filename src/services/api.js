732c5dc55fb2edaa99d83db8af6cda8e7ead4bb7
Maicon Augusto <maicon.augusto.carvalho@hotmail.com>
2020-12-07 15:19:13 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,3 @@
 import axios from 'axios';
-const api = axios.create({baseURL: 'https://api-prod-gestao.herokuapp.com'});
+const api = axios.create({baseURL: process.env.HOST'});
 export default api;
-
-// const api = axios.create({baseURL: process.env.URL || 'http://localhost:8080'});
